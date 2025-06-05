1e882618340c78e86ab4e0b07b1d73dc603b02d2
harshvardhanchand <harshvardhanchand.ug21@nsut.ac.in>
2025-06-05 21:33:18 +0530

    rebuild

--- app.config.js
+++ app.config.js
@@ -24,6 +24,7 @@ export default {
     }
     },
     "android": {
+      "package": "com.medicaldatahub",
       "adaptiveIcon": {
         "foregroundImage": "./frontend/assets/adaptive-icon.png",
         "backgroundColor": "#ffffff"
