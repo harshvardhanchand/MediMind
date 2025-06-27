884c1d223473c86de32b4e698ac204b6e9b77c2f
harshvardhanchand <harshvardhanchand.ug21@nsut.ac.in>
2025-06-27 22:20:22 +0530

    name change

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 export default {
   "expo": {
     "name": "MediMind",
-    "slug": "medimind",
+    "slug": "medical-data-hub",
     "version": "1.0.0",
     "orientation": "portrait",
     "icon": "./frontend/assets/icon.png",
